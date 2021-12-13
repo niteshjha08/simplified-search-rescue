13b62a9ffbee4348528bc462195c4b41a80237b1
Naveen Mangla <nmangla@umd.edu>
2021-12-13 00:03:49 -0500

    follower motion

--- src/test.cpp
+++ src/test.cpp
@@ -51,6 +51,23 @@ void move_to_target(XmlRpc::XmlRpcValue aruco_lookup_location) {
   explorer_client.waitForResult();
 
 }
+void follow_to_target(std::vector<double>follow) {
+  // ROS_ASSERT(aruco_lookup_location.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+  move_base_msgs::MoveBaseGoal follower_goal;
+  follower_goal.target_pose.header.frame_id = "map";
+  follower_goal.target_pose.header.stamp = ros::Time::now();
+  follower_goal.target_pose.pose.position.x = follow[1]-0.25;//
+  follower_goal.target_pose.pose.position.y = follow[2]-0.25;//
+  follower_goal.target_pose.pose.orientation.w = 1.0;
+  MoveBaseClient follower_client("/follower/move_base", true);
+  while (!follower_client.waitForServer(ros::Duration(5.0))) {
+    ROS_INFO("Waiting for the move_base action server to come up for explorer");
+  }
+  follower_client.sendGoal(follower_goal);
+  follower_client.waitForResult();
+
+}
 
 void search_at_target(ros::Publisher explorer_rotator){
     aruco_found=false;
@@ -178,17 +195,10 @@ int main(int argc, char** argv) {
   nh.getParam("/aruco_lookup_locations/target_4", aruco_lookup_locations);
   move_to_target(aruco_lookup_locations);
   search_at_target(explorer_rotator);
-  // base();
-  // move_base_msgs::MoveBaseGoal explorer_goal;
-  // ROS_INFO("Moving back to base bitches!!!!!  ");
-  // explorer_goal.target_pose.header.frame_id = "map";
-  // explorer_goal.target_pose.header.stamp = ros::Time::now();
-  // explorer_goal.target_pose.pose.position.x = -4;//
-  // explorer_goal.target_pose.pose.position.y = 2.5;//
-  // explorer_goal.target_pose.pose.orientation.w = 1.0;
-  // MoveBaseClient explorer_client("/explorer/move_base", true);
-  // explorer_client.sendGoal(explorer_goal);
-  // explorer_client.waitForResult();
+  
+  base();
+
+  
 
   std::cout << "locations:";
   for (int i{}; i < 4 ; i++) {
@@ -206,8 +216,9 @@ int main(int argc, char** argv) {
     }
     std::cout << '\n';
   }
-  
-
+  for (int i{};i < 4;i++) {
+    follow_to_target(fiducial_array[i]);
+  }
   // ros::spin();
 
 }
